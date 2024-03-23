6e9b872d3b62e0e044a860334b6071b05dea94aa
lyuwenyi <lyuwenyi@gmail.com>
2024-03-24 00:39:33 +0800

    Add:3083. Existence of a Substring in a String and Its Reverse

---
+++ Code/CountSubtringsAndEndingWithGivenCharacter.java
@@ -0,0 +1,11 @@
+class CountSubtringsAndEndingWithGivenCharacter {
+    public long countSubstrings(String s, char c) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==c){
+                count++;
+            }
+        }
+        return (count+1L)*count/2;
+    }
+}
