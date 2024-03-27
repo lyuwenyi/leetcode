b0ae73f6e953513083b27aaf36bd205bbe0fb888
lyuwenyi <lyuwenyi@gmail.com>
2024-03-27 21:48:38 +0800

    Add:upload the answer of a new problem

--- Code/CountSubtringsAndEndingWithGivenCharacter.java
+++ Code/CountSubtringsAndEndingWithGivenCharacter.java
@@ -1,11 +1,11 @@
 class CountSubtringsAndEndingWithGivenCharacter {
     public long countSubstrings(String s, char c) {
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==c){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
                 count++;
             }
         }
-        return (count+1L)*count/2;
+        return (count + 1L) * count / 2;
     }
 }
